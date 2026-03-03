ddc7ca7ee0d986b734a2456afb760779feebd042
Amorós Mèlich <oamoros3@edu.gencat.cat>
2026-03-03 11:09:41 +0100

    commit

--- analitzar_csv_v2.R
+++ analitzar_csv_v2.R
@@ -2,8 +2,15 @@
 library(tidyverse)
 library(sf) 
 
-df2 <- data.frame(
-  id = c(5,6,7,8),
-  nom = c("Fluvià","Muga","mmm","ooo"),
-  longitud = c(80, 65, 89,98)
+rius <- data.frame(
+  id = 1:10,
+  nom = c("Riu_A","Riu_B","Riu_C","Riu_D","Riu_E",
+          "Riu_F","Riu_G","Riu_H","Riu_I","Riu_J"),
+  longitud = c(56, 56,
+               44, 44,
+               120, 120,
+               80, 80,
+               95, 200)
 )
+
+rius
