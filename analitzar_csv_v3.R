cdbcb6fb04efb8d38138beca5a6c4f783cea94b9
Amorós Mèlich <oamoros3@edu.gencat.cat>
2026-03-04 11:45:44 +0100

    commit

---
+++ analitzar_csv_v3.R
@@ -0,0 +1,14 @@
+
+#    PROJECTE 
+#    
+#    Automatizar el procés de depuració de CSV   
+#    La ideea poder bolcar el CSV de TOTS els rius de QGIS
+#    I que automaticament els SENSE NOMS els hi dongui un nom
+
+
+library(tidyverse)
+library(sf) 
+
+#    Crear una TAULA amb 4 COLUMNES
+#    Dos columnes de ID i Dos de NOM
+#    Ha d'haver noms SENSE NOM per k jo aprengui a depurar
