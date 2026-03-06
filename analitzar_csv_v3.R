3b62f62f5cd4dddc6f7afa4f9fd4bae9ba4b4810
Amorós Mèlich <oamoros3@edu.gencat.cat>
2026-03-06 12:29:11 +0100

    commit

--- analitzar_csv_v3.R
+++ analitzar_csv_v3.R
@@ -24,13 +24,38 @@ names(rius)
 #   Vull DETECTAR els ID_2 REPETITS
 #   ID_2 REPETITS = vol dir que un RIU SIN NOMBRE ha fet INTERSECT amb RIU AMB NOM (ID)
 
-#   ORDENAR una taula x ID_2
-#   Així puc veure les dades ordenades
-
-rius %>%
-  arrange(OBJECTID_2)
-
 #   ANALIZAR
 
-long <- length(rius$nom_rio)
+#   Creo NOVA TAULA ORDENAD  x ID_2
+#   Així podré comparar els ID_2 un darrere l'altre
+
+rius_2 <- rius %>%
+  arrange(OBJECTID_2)
+
+#   Creo NOVA TAULA ORDENAD  x ID_2
+#   Així podré comparar els ID_2 un darrere l'altre
+
+long <- length(rius_2$nom_rio)
+
+rius_nom_nou <- data.frame()
+for (i in 1:(long-1)){
+  id_1 <- rius_2$OBJECTID_2[i]
+  id_2 <- rius_2$OBJECTID_2[i+1]
+  
+  if(id_1 == id_2 ){
+    id_sinnom <- id_1
+    nou_nom <- rius_2$nom_rio[i]
+    
+    rius_nom_nou <- rbind(
+      rius_nom_nou,
+      data.frame(OBJECTID = id_sinnom, nom_rio = nou_nom)
+    )
+    
+  }
+  
+  
+}
+
+
+
 
