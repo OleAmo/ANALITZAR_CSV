f2e3ec96044aeae4025f8ae9677c99ce85954537
Oleguer <oleguer.amoros@gmail.com>
2026-03-12 17:55:28 +0100

    commit

--- analitzar_csv_v4.R
+++ analitzar_csv_v4.R
@@ -32,9 +32,12 @@ netja_csv <- function(rius) {
   nom <- rius_2[,2]
   id_2 <- rius_2[,3]
   nom_2 <- rius_2[,4]
-
-  long <- length(id)              # Comparo si ID_2 es IGUAL al seguent ID_2
-  rius_sinnom <- data.frame()     # Si es així poso el ID_2 i el NOM els poso en un DATAFRAME
+  
+            # Comparo si ID_2 es IGUAL al seguent ID_2
+            # Si es així poso el ID_2 i el NOM els poso en un DATAFRAME
+ 
+   long <- length(id)              
+  rius_sinnom <- data.frame()    
   for (i in 1:(long-1)){
     id__1 <- id_2[i]
     id__2 <- id_2[i+1]
@@ -50,9 +53,7 @@ netja_csv <- function(rius) {
     }
   }
   
-  
   # Faig NOVA ITERACIÓ de RIUS_SINOM per NETEJAR els ID REPETITS
-  
  
   long_2 <- length(rius_sinnom[,1])         
   rius_sinnom_2 <- data.frame()
@@ -70,15 +71,57 @@ netja_csv <- function(rius) {
         data.frame(OBJECTID = id_sinnom_2, nom_rio = nou_nom_2)
       )
     }
-    
-    
   }
+  
+  #   Ara que ja tinc RIUS_SINOM_2 una TAULA de ID amb NOMS
+  #   Ara que ja tinc RIUS_SINOM una TAULA de ID REPETITS amb SENSE NOMS
+  #   He de crear un TUALA RIUS_SINOM_3
+  #   Aquesta taula serà els la RIUS_SINOM eliminant els ID REPETITS  
+  #   Justament els repetits son els ID de RIUS_SINOM_2
+  #   Un cop eliminats li afegirem els ID de RIUS_SIOM_2
+  
+  rius_sinnom_3 <- rius_sinnom %>%
+    filter(!(rius_sinnom$OBJECTID %in% rius_sinnom_2$OBJECTID)) 
+  
+  long <- length(rius_sinnom_2$OBJECTID)
+  for (i in 1:long ){
+    
+    rius_sinnom_3 <- rbind(
+      rius_sinnom_3,
+      data.frame(OBJECTID = rius_sinnom_2$OBJECTID[i], 
+                 nom_rio = rius_sinnom_2$nom_rio[i])
+    )
+  }
+  
+  rius_sinnom_3 <- rius_sinnom_3 %>%     # Ordeno per OBJECTID
+    arrange(OBJECTID)
+  
+  rius <- rius %>%                       # Ordeno per OBJECTID
+    arrange(OBJECTID)
+  
+  
+  llista <- list(
+    df_1 =rius_sinnom,
+    df_2 = rius_sinnom_2,
+    df_FINAL = rius_sinnom_3,
+    df_ORIGINAL = rius
+  )
 
  
-  return(rius_sinnom_2)
+  return(llista)
 }
 
-netja_csv(rius)
+llista <- netja_csv(rius)
+
+llista$df_1
+llista$df_2
+llista$df_FINAL
+llista$df_ORIGINAL
+
+ 
+
+
+
 
 
 #   prova
