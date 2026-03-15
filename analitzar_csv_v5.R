5b397ef039cb728f494d86497858c6d9ca1381a1
Oleguer <oleguer.amoros@gmail.com>
2026-03-15 19:53:18 +0100

    commit

--- analitzar_csv_v5.R
+++ analitzar_csv_v5.R
@@ -36,24 +36,35 @@ netja_csv <- function(rius) {
   #   PRIMER = netejar els ID_1 de ID_2
   #   Els ID_1 són BARREJA de RIUS amb NOM i RIUS sense NOM
   #   Els ID_2 són NOMÉS RIUS SENSE NOM
-  #   Pertant lo primer es CREAR una TAULA amb els RIUS amb NOM
-  #   Així al final de tot quan tingui la TAULA dels SENSE NOM (ara amb nom), els unire a aquesta taula creada
-  #   I així finalment tindre RIUS AMB NOM. Tant els que ja en tenien com el SENSE NOM que li he donat nom
+  #   Pertant lo primer es CREAR una TAULA (rius_2_nom) = [ ID_nom = ID_1 - ID_2 ] 
+  #   Aqusta taula son els ID amb NOM ORIGINAL
+  #   La resta de IDs son SENSE NOM que han d'agafar nom del algun ID NOM ORIGINAL
   
-  #   PROCÉS:
+  #   ABANS DE TOT:
+  
+  #   Vull saber quants RIUS amb NOM tinc
+  #   Vull saber quants RIUS SENSE NOM tinc
+  
+  #   RIUS AMB NOM:
+  
   #   Elimino de RIUS_2 els IDs que son iguals a ID_2
   #   Així tindre la taula de RIUS amb NOM que ja venen així del CSV
   #   Un cop fet DEIXO els VALORS ÚNICS
   
-  rius_2_nom <- rius_2 %>%
+  rius_2_AMB_NOM <- rius_2 %>%
     filter(!(rius_2$OBJECTID %in% rius_2$OBJECTID_2)) 
   
-  rius_2_nom <- rius_2_nom %>%              # Elimino els ID repetits
+  rius_2_AMB_NOM <- rius_2_AMB_NOM %>%      # Elimino els ID repetits
     distinct(OBJECTID, .keep_all = TRUE)    # I deixo un sola fila dels IDs repetits
                                             # ho ha sabut fer gràcies a CHATGPT 
    
+  
+  id_amb_nom <- rius_2_AMB_NOM$OBJECTID
+  id_sense_nom <- unique(rius_2$OBJECTID_2)
+  
+  
   #   SEGÜENT PROCÉS:
-  #   Un cop creada la TAULA amb NOM ORIGINAL = RIUS_2_NOM
+  #   Un cop creada la TAULA amb NOM ORIGINAL = rius_2_AMB_NOM
   #   Ara ja jodem ANARLITZAR els ID_2 per donar NOM als SENSE NOM
   
   #   Comparo si ID_2 es IGUAL al seguent ID_2
@@ -105,9 +116,11 @@ netja_csv <- function(rius) {
   llista <- list(
     id_1 =id,
     id_2 =id_2,
+    id_amb_nom = id_amb_nom,
+    id_sense_nom = id_sense_nom,
     df_1 =rius_sinnom,
     df_2 =rius_sinnom_2,
-    ORIGINAL_nom = rius_2_nom,
+    ORIGINAL_AMB_NOM = rius_2_AMB_NOM,
     ORIGINAL = rius_2
   )
 
@@ -121,8 +134,9 @@ unique(llista$id_2)
 
 
 llista$ORIGINAL
+llista$ORIGINAL_AMB_NOM
 llista$df_1
 llista$df_2
-llista$ORIGINAL_nom
+
 
   
